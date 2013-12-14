df1ff5cd1fca7ff47a791a543f89b1b4b07c80e9
Rome Arnold <romearnold@gmail.com>
2013-12-14 01:17:49 -0500

    Initial commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,8 @@
+class WelcomeController < ApplicationController
+
+	def index
+		@name = "Rome George Arnold IV"
+
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '7bc1c122c00ada3e9706f5bc617edd73e590a3ccfb0256358434b2f681b502284383787596de49dc4fb0097ac3955cb7c8327d1b5397685adc5176ee03ac3fc2'
